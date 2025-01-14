7b821ea61954451650d3ef2da5ff5e982bc34de2
Nathalia <nathaliamend017@gmail.com>
2025-01-14 11:34:25 -0400

    Primeira aula do nivelamento conhecendo o TypeScript

---
+++ TypeScript/Conhecendo.ts
@@ -0,0 +1,15 @@
+// O JavaScript é uma linguagem não-tipada. O TypeCript adiciona tipagem no JavaScript e depois ele se torna JavaScript. O TypeScript foi criado e adotado pela microsoft que é a mesma criadora do vscode.
+/*
+    Vantagens de utilizar o TypeScript:
+
+    *Feedback mais rapido de erros;
+    *Antecipa erros que seriam vistos em execucao;
+    *Ajuda a manter o codigo consistente;
+    *Ajuda no trabalho em times;
+    *Processo de refatoracao mais facil;
+    *Recurso de Autocomplete;
+    *Pode adotar gradualmente typescrpit em uma base de codigo;
+
+    //Para testes com TypeScript : https://www.typescriptlang.org/es/play/?#code/PTAEEKCEEsFMDsBuDoBMD2oCGAbUtVoAXdAJ33lB2nli1FVlABUBPAB1gGUBjU6dkQCEAGnwBnIk1jjQAV0rji0TAHdYAI1AAoEKACOcplNAb+8VPRz107MkQVosqBtPF9oG6KTE90AW3YsUiJvUFZsdlIERnI2Tl5+QQA6bV0wNL0ABSNGWU54cWCcTEZQImjZfyxaUixxAC509NBQAFpQAFVFZUwgusjoi1g4jm4PQVdQatr6cVgAczk6lvauntC+4PpYAA9Ofn8EImDQP0prUCV4E93oWTL48aSicLOA-tDyBzOAZ8IFphzqB0BV0OJUno1t0rr1QP0dvsRtAjjdTsDrA9oAAzEbHGRnbY8KT8STQHj1Vx4PyBaDWDDkR5jRICIhpc6SbDwACiuywgRwsAAasFoFgNILQABeUAAIgAErAcCVQAB1Mg4VCy7Qc9CC5IlBYAChqvP57EFIv44sFAEpMmAsttBjERtMAIeyLA8EYUqlUBBLLAAK1gYgAFlgAF5nag8ADWFCo9E+THEcgOKnICDl3NDAvBspBcoA-QBFOQAS9AkYiZXgRkQ6AA-LLIWAACJMFXnCqi9C+AJweBRnbUfw0GoYCQkWQ04798RiCJK6YKacEn7u6L0MrBoyA8RCbStVpAA
+
+ */
