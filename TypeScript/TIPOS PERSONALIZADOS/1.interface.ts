45a88ced50baec9a904a73288131f6f9d651058e
Nathalia <nathaliamend017@gmail.com>
2025-01-16 09:59:58 -0400

    conhecendo interfaces

---
+++ TypeScript/TIPOS PERSONALIZADOS/1.interface.ts
@@ -0,0 +1,12 @@
+// como utilizar infaces?
+
+interface Product{
+    id: number,
+    name: string
+}
+
+function newProduct(product: Product){
+    console.log()
+}
+
+newProduct({id: 1, name: "novalgina"})
