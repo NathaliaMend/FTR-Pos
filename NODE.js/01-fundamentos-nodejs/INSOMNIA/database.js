d0937440b03f70c8f44a76f564f63af711819077
Nathalia <nathaliamend017@gmail.com>
2025-02-02 12:01:43 -0400

    criando um banco de dados

---
+++ NODE.js/01-fundamentos-nodejs/INSOMNIA/database.js
@@ -0,0 +1,15 @@
+export class Database {
+    #database = {}
+    
+    select(table) {
+        const data = this.#database[table] ?? []
+        return data
+    }
+    insert(table, data) {
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push
+        } else {
+            this.#database[table] = data
+        }
+    }
+}
