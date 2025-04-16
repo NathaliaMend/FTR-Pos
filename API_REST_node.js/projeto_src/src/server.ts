68d9ea8d4249f18e0615ec82d0091d6f104c5c75
Nathalia <nathaliamend017@gmail.com>
2025-04-16 19:14:58 -0400

    realizando queries com knex.js

--- API_REST_node.js/projeto_src/src/server.ts
+++ API_REST_node.js/projeto_src/src/server.ts
@@ -1,12 +1,31 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
+// INSERINDO
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
+// SELECIONANDO
+/*
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
+})
+  */
 
 app
   .listen({
