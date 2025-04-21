dbbae557193424d2303ddd2058dae267f30e3925
Nathalia <nathaliamend017@gmail.com>
2025-04-21 16:23:27 -0400

    listagem das transações

--- API_REST_node.js/projeto_src/src/routes/transactions.ts
+++ API_REST_node.js/projeto_src/src/routes/transactions.ts
@@ -3,8 +3,25 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
-// Request body : dados geralmente preenchido atraves de um formulario
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return {
+      total: 200,
+      transactions,
+    }
+  })
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
+  // Request body : dados geralmente preenchido atraves de um formulario
   app.post('/', async (request, reply) => {
     // {title, amount, type: credit ou debit} . Validando :
 
