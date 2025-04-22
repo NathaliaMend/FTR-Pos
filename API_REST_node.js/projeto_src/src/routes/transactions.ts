315dc6c5cdf8d0de47c1153b6eff2d994257520d
Nathalia <nathaliamend017@gmail.com>
2025-04-22 09:46:48 -0400

    instalação do cookies com fastify e configuração para utiliza-lo

--- API_REST_node.js/projeto_src/src/routes/transactions.ts
+++ API_REST_node.js/projeto_src/src/routes/transactions.ts
@@ -42,10 +42,21 @@ export async function transactionsRoutes(app: FastifyInstance) {
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days em segundos
+      })
+    }
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
     // http codes - 201 -> sucess
     return reply.status(201).send()
