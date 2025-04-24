03e6fc073bdb81399431fe246817cac8cb5996c5
Nathalia <nathaliamend017@gmail.com>
2025-04-24 15:57:30 -0400

    testando listagem de transações

--- API_REST_node.js/projeto_src/test/transactions.spec.ts
+++ API_REST_node.js/projeto_src/test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, it, describe } from 'vitest'
+import { afterAll, beforeAll, it, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -29,5 +29,31 @@ describe('Transactions routes', () => {
       })
       .expect(201)
     // ou expect(response.statusCode).toEqual(201)
+  })
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    if (!cookies) {
+      throw new Error('No cookies found in the response')
+    }
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+    console.log(listTransactionsResponse.body)
   })
 })
