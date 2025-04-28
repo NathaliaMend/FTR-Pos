997569c91fcda41164365dd3d85577a02f35c6cf
Nathalia <nathaliamend017@gmail.com>
2025-04-28 17:33:00 -0400

    adicionado host para correcao de erro no deploy

--- API_REST_node.js/projeto_src/src/server.ts
+++ API_REST_node.js/projeto_src/src/server.ts
@@ -14,9 +14,11 @@
 import { app } from './app'
 import { env } from './env'
 
+const host = 'RENDER' in process.env ? `0.0.0.0` : `localhost`
 app
   .listen({
     port: env.PORT,
+    host,
   })
   .then(() => {
     console.log('HTTP Server Running!')
